2ca0343dffd5ed34c5a26fcf604c7880ae72b70f
context-coding <context.coding@gmail.com>
2022-03-21 12:55:16 -0500

    update

---
+++ src/pages/index/index.js
@@ -0,0 +1,10 @@
+import style from './index.css';
+import component from "../../components/Component.js";
+
+console.log('¡Hola!');
+console.log('adios');
+console.log('word');
+
+document.body.append(component());
+
+console.log("appending base element in head...");
